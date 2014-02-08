6d3a433bffe5372c0bb097e18d40bf87af2323e7
jbeuckm <joe@beigerecords.com>
2014-02-07 21:57:22 -0600

    using commander to run the tests

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+
+program
+  .version('0.0.1')
+  .option('-c, --config', 'JSON file that configures the test')
+//  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .parse(process.argv);
+
+if (!program.config) {
+    console.log("You must supply a config file.");
+    process.exit(-1);
+}
+
+
+
 var cp = require('child_process');
 var fs = require('fs');
 
---
+++ example/test.sh
@@ -0,0 +1,1 @@
+../app.js -c config.json 
