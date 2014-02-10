f8186fdf6b84d1079c957756dc1045538178c381
joe beuckman <joebeuckman@gmail.com>
2014-02-09 22:01:00 -0600

    new fitness function

--- examples/pcl/analyzer.js
+++ examples/pcl/analyzer.js
@@ -3,9 +3,27 @@ exports.analyze = function(command, set_combination, arg_combination, stdout, st
 
   var result;
   try {
+
+    var model = parseInt(set_combination[0]);
+    var scene = set_combination[1].split('_').map(parseInt);
+
     result = JSON.parse(stdout);
 
-    var fitness = result.instances;
+    var fitness;
+
+
+    if (scene.indexOf(model) != -1) { // model is in the scene
+      fitness = result.instances;
+    }
+    else { // // model is NOT in the scene
+      if (result.instaces == 0) {
+        fitness = .5; // award non-false-positive
+      }
+      else {
+        fitness = -.5 * result.instances; // punish false-positive
+      }
+    }
+
     if (model_keypoints > 10) {
       fitness += .1 * result.correspondences/result.model_keypoints;
     }
