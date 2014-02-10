8e9b667f703fa5f4f601c0ce276992246a29a7f9
jbeuckm <joe@beigerecords.com>
2014-02-09 22:23:24 -0600

    updated config

--- examples/pcl/analyzer.js
+++ examples/pcl/analyzer.js
@@ -4,16 +4,27 @@ exports.analyze = function(command, set_combination, arg_combination, stdout, st
   var result;
   try {
 
-    var model = parseInt(set_combination[0]);
-    var scene = set_combination[1].split('_').map(parseInt);
+      var parts = set_combination[0].split('/');
+      var model_file = parts[parts.length-1];
+      var model = parseInt(model_file.replace('.pcd',''));
+
+      parts = set_combination[1].split('/');
+      var scene_file = parts[parts.length-1].replace('.pcd','');
+      var scene_parts = scene_file.split('_');
+      var scene = [];
+      for (var i=0; i<scene_parts.length; i++) {
+	  scene.push(parseInt(scene_parts[i]));
+      }
 
     result = JSON.parse(stdout);
 
     var fitness;
 
 
+      var modelInScene = false;
     if (scene.indexOf(model) != -1) { // model is in the scene
       fitness = result.instances;
+	modelInScene = true;
     }
     else { // // model is NOT in the scene
       if (result.instaces == 0) {
@@ -24,12 +35,14 @@ exports.analyze = function(command, set_combination, arg_combination, stdout, st
       }
     }
 
-    if (model_keypoints > 10) {
+    if (result.model_keypoints > 10) {
       fitness += .1 * result.correspondences/result.model_keypoints;
     }
 
     return {
-      fitness: fitness
+	modelInScene: modelInScene,
+        instances: result.instances,
+	fitness: fitness
     };
 
   }
